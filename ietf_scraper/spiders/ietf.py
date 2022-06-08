19ee78b604d6645174c96a48fa742c2b2993acd3
adityak <aditya.py9@gmail.com>
2022-06-08 17:42:01 +0530

    changes in ietf.py

--- ietf_scraper/spiders/ietf.py
+++ ietf_scraper/spiders/ietf.py
@@ -1,10 +1,17 @@
+import imp
+import os 
+import tkinter
+import turtle
+from turtle import title
 import scrapy
 
 
 class IetfSpider(scrapy.Spider):
     name = 'ietf'
     allowed_domains = ['pythonscraping.com']
-    start_urls = ['http://pythonscraping.com/']
+    start_urls = ['http://pythonscraping.com/linkedin/ietf.html']
 
     def parse(self, response):
-        pass
+        # title = response.css('span.title::text').get()
+        title = response.xpath('//span[@class="title"]/text()').get()
+        return {"title": title}
