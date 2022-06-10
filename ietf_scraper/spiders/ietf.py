462005c3c3ba2c8c5d9f8509d0acf3861e111dc5
adityak <aditya.py9@gmail.com>
2022-06-10 15:43:48 +0530

    updating ietf.py scrapping all data

--- ietf_scraper/spiders/ietf.py
+++ ietf_scraper/spiders/ietf.py
@@ -12,6 +12,15 @@ class IetfSpider(scrapy.Spider):
     start_urls = ['http://pythonscraping.com/linkedin/ietf.html']
 
     def parse(self, response):
+        return{
+        'number' : response.xpath('//span@class="rfc-no"]/text()').get(),
         # title = response.css('span.title::text').get()
-        title = response.xpath('//span[@class="title"]/text()').get()
-        return {"title": title}
+       'title' : response.xpath('//span[@class="title"]/text()').get(),
+       'date' : response.xpath('//span[@class="date"]/text()').get(),
+       'description': response.xpath('//meta[@name="DC.Description.Abstract"]/@content').get(),
+       'author' : response.xpath('//meta[@name="DC.Creator"]/@content').get(),
+       'company': response.xpath('//span[@class="author-company"]/text()').get(),
+       'address': response.xpath('//span[@class="address"]/text()').get(),
+       'text': w3lib.html.remove_tags(response.xpath('//div[@class="text"]').get()),
+       'headings':response.xpath('//span[@class="subheading"]/text()').getall()
+    }    
